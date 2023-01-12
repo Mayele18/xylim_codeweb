0c1e3870655dc66fe99bdaedcb54f6748a7f2750
Mayele18 <alfredojoaodacruzdacruz@gmail.com>
2023-01-12 00:08:31 +0000

    add7

---
+++ js2/t.js
@@ -0,0 +1,13 @@
+function mudaft(foto){
+    document.getElementById("idm").src=foto;
+}
+
+
+function mudaft1(foto){
+    document.getElementById("idm1").src=foto;
+}
+
+function comprar(){
+    alert("Neste momento as compras não são feitas online mas por pedido.Os pedidos podem ser feitos pelo:email-Lizzokidd081@gmail.com.Chamada e watsapp:923411126-willtold Francisco.Insta:Xylim18.O tempo de espera normal é de 48 dias caso não estiver em stock varia de 5-7 dias.");
+
+}
