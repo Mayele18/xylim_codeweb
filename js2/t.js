2009eb7502e7099ee8777f6e7085c04d3d0d2309
Mayele18 <alfredojoaodacruzdacruz@gmail.com>
2023-01-13 07:57:47 +0000

    cfinal

--- js2/t.js
+++ js2/t.js
@@ -1,13 +1,68 @@
-function mudaft(foto){
+function mudaft(foto,fto2,fto3,fto4,fto5,fto6,fto7,fto8,fto9,fto10,fto11,fto12){
     document.getElementById("idm").src=foto;
+    document.getElementById("idm2").src=fto2;
+    document.getElementById("idm3").src=fto3;
+    document.getElementById("idm4").src=fto4;
+    document.getElementById("idm5").src=fto5;
+    document.getElementById("idm6").src=fto6;
+    document.getElementById("idm7").src=fto7;
+    document.getElementById("idm8").src=fto8;
+    document.getElementById("idm9").src=fto9;
+    document.getElementById("idm10").src=fto10;
+    document.getElementById("idm11").src=fto11;
+    document.getElementById("idm12").src=fto12;
+    document.getElementById("mdp").innerHTML="Preço:16.000.00kzs";
+    document.getElementById("mdp2").innerHTML="Preço:25.000.00kzs";
+    document.getElementById("mdp3").innerHTML="Preço:22.000.00kzs";
+    document.getElementById("mdp4").innerHTML="Preço:22.000.00kzs";
+    document.getElementById("mdp5").innerHTML="Preço:14.000.00kzs";
+    document.getElementById("mdp6").innerHTML="Preço:13.000.00kzs";
+    document.getElementById("mdp7").innerHTML="Preço:9.000.00kzs";
+    document.getElementById("mdp8").innerHTML="Preço:15.000.00kzs";
+    document.getElementById("mdp9").innerHTML="Preço:21.000.00kzs";
+    document.getElementById("mdp10").innerHTML="Preço:17.500.00kzs";
+    document.getElementById("mdp11").innerHTML="Preço:Indisponivel";
+    document.getElementById("mdp12").innerHTML="Preço:Indisponivel";
 }
 
 
-function mudaft1(foto){
-    document.getElementById("idm1").src=foto;
+function mudaft2(foto,fto2,fto3,fto4,fto5,fto6,fto7,fto8,fto9,fto10,fto11,fto12){
+    document.getElementById("idm").src=foto;
+    document.getElementById("idm2").src=fto2;
+    document.getElementById("idm3").src=fto3;
+    document.getElementById("idm4").src=fto4;
+    document.getElementById("idm5").src=fto5;
+    document.getElementById("idm6").src=fto6;
+    document.getElementById("idm7").src=fto7;
+    document.getElementById("idm8").src=fto8;
+    document.getElementById("idm9").src=fto9;
+    document.getElementById("idm10").src=fto10;
+    document.getElementById("idm11").src=fto11;
+    document.getElementById("idm12").src=fto12;
+    document.getElementById("mdp").innerHTML="Preço:8.000.00kzs";
+    document.getElementById("mdp2").innerHTML="Preço:6.500.00kzs";
+    document.getElementById("mdp3").innerHTML="Preço:5.000.00kzs";
+    document.getElementById("mdp4").innerHTML="Preço:10.000.00kzs";
+    document.getElementById("mdp5").innerHTML="Preço:5.500.00kzs";
+    document.getElementById("mdp6").innerHTML="Preço:10.000.00kzs";
+    document.getElementById("mdp7").innerHTML="Preço:7.000.00kzs";
+    document.getElementById("mdp8").innerHTML="Preço:10.000.00kzs";
+    document.getElementById("mdp9").innerHTML="Preço:6.000.00kzs";
+    document.getElementById("mdp10").innerHTML="Preço:13.000.00kzs";
+    document.getElementById("mdp11").innerHTML="Preço:9.000.00kzs";
+    document.getElementById("mdp12").innerHTML="Preço:6.000.00kzs";
 }
+
+
+function compr(tr){
+    document.getElementById("trid").src=tr;
+}
+
+function compr2(tr2){
+    document.getElementById("trid2").src=tr2;
+}
+
 
 function comprar(){
     alert("Neste momento as compras não são feitas online mas por pedido.Os pedidos podem ser feitos pelo:email-Lizzokidd081@gmail.com.Chamada e watsapp:923411126-willtold Francisco.Insta:Xylim18.O tempo de espera normal é de 48 dias caso não estiver em stock varia de 5-7 dias.");
-
 }
